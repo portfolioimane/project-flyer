a131263332b757277d794e62931f1c478cc430f7
insert github real name here <insert github email here>
2020-10-22 20:40:41 +0100

    edit this files

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Project Flyer</title>
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+	 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+	<div class="container">
+		@yield('content')
+	</div>
+</body>
+</html>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+@section('content')
+   <div class="jumbotron">
+    <h1>Project flyer</h1>
+    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+    <button class="btn btn-primary">Create a Flyer</button>
+  </div>
+@stop
